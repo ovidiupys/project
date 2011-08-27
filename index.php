fa2973d10fdb4d5153b6c321a961ef4489a696a9
Ovidiu Ion <ovidiu.na@gmail.com>
2011-08-27 13:53:02 +0300

    modified the index.php file

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-<?php echo 'the PHP file'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+<title>test</title></head>
+<body>
+<div>
+
+<?php echo 'the PHP file in the master branch'; ?>
+
+</div>
+</body>
+</html>

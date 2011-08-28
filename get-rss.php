edcde026b70f09fd23c78a7e56b1386baaba97c5
Ovidiu Ion <ovidiu.na@gmail.com>
2011-08-28 10:10:34 +0300

    added get-rss.php and modified index.php

---
+++ get-rss.php
@@ -0,0 +1,27 @@
+<?php 
+
+$way = $_GET['way'];
+
+if(!in_array($way,$ways){$rezultat = 'err';}
+else{
+switch($way)
+	{
+		case "easy" :
+  $doc = new DOMDocument();
+  $doc->load('http://weather.yahooapis.com/forecastrss?p=ROXX0008&u=c');
+  $arrFeeds = array();
+  foreach ($doc->getElementsByTagName('item') as $node) {
+    $itemRSS = array ( 
+      'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+      'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+      'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+      'date' => $node->getElementsByTagName('pubDate')->item(0)->nodeValue
+      );
+    array_push($arrFeeds, $itemRSS);
+  } ; break;
+		case "complex" : ; break;
+	}
+}
+print_r($arrFeeds);
+//echo $rezultat;
+?>
--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-<?php echo 'the PHP file in the php branch'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <script src="http://code.jquery.com/jquery-latest.js"></script>
+<title>test</title></head>
+<body>
+<div style="text-align:center;">
+<h1>Two ways of getting some data from an rss link</h1>
+<script>
+$(document).ready(function() 
+{
+	var loadUrl = "get-rss.php";
+	$("#easy").click(function(){  
+    	$("#result")  
+        .html(ajax_load)  
+        .load(loadUrl, "language=php&version=5");  
+	});
+});
+</script>
+<h2><a href="" id="easy">The easy way</a></h2>
+
+<h2><a href="">The complex way</a></h2>
+
+</div>
+<div id="result"></div>
+</body>
+</html>

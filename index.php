b389786cbfd94b367938b12c968f96d2e15b7f83
Ovidiu Ion <ovidiu.na@gmail.com>
2011-08-28 10:18:49 +0300

    simplified index.php

--- index.php
+++ index.php
@@ -6,22 +6,10 @@
 <body>
 <div style="text-align:center;">
 <h1>Two ways of getting some data from an rss link</h1>
-<script>
-$(document).ready(function() 
-{
-	var loadUrl = "get-rss.php";
-	$("#easy").click(function(){  
-    	$("#result")  
-        .html(ajax_load)  
-        .load(loadUrl, "way=easy");  
-	});
-});
-</script>
-<h2><a href="" id="easy">The easy way</a></h2>
+<h2><a href="get-rss.php?way=easy" id="easy">The easy way</a></h2>
 
-<h2><a href="">The complex way</a></h2>
+<h2><a href="get-rss.php?way=complex">The complex way</a></h2>
 
 </div>
-<div id="result"></div>
 </body>
 </html>
